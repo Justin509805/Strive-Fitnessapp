e1b66c8dfc72d243d645a5de016def9f008ea04d
Justin509805 <509805@student.fontys.nl>
2024-12-29 16:12:57 +0100

    fixed the permit of a description

--- darkmode.js
+++ darkmode.js
@@ -1,43 +1,45 @@
 /**
- * Checks and sets the 'darkmode' state in local storage.
- * @type {string | null} Current state of dark mode ('active' or null).
- */
+* The current state of dark mode ('active' or null).
+* Retrieved from local storage.
+* @type {string | null}
+*/
 let darkmode = localStorage.getItem('darkmode');
 
 /**
- * The theme switcher button element.
- * @type {HTMLElement | null}
- */
+* The theme switcher button element.
+* If the element is not found, this will be null.
+* @type {HTMLElement | null}
+*/
 const themeSwitch = document.getElementById('theme-switch');
 
 /**
- * Enables dark mode by adding the 'darkmode' class to the document body
- * and updating the local storage.
- */
+* Enables dark mode by adding the 'darkmode' class to the document body
+* and updating the local storage.
+*/
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode');
-    localStorage.setItem('darkmode', 'active');
+   document.body.classList.add('darkmode');
+   localStorage.setItem('darkmode', 'active');
 };
 
 /**
- * Disables dark mode by removing the 'darkmode' class from the document body
- * and updating the local storage.
- */
+* Disables dark mode by removing the 'darkmode' class from the document body
+* and updating the local storage.
+*/
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode');
-    localStorage.setItem('darkmode', null);
+   document.body.classList.remove('darkmode');
+   localStorage.setItem('darkmode', null);
 };
 
 /**
- * Checks if dark mode is active when the page loads
- * and applies the dark mode if it is enabled.
- */
+* Checks if dark mode is active when the page loads
+* and applies the dark mode if it is enabled.
+*/
 if (darkmode === "active") enableDarkmode();
 
 /**
- * Toggles dark mode on and off when the theme switch button is clicked.
- */
+* Toggles dark mode on and off when the theme switch button is clicked.
+*/
 themeSwitch?.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode');
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+   darkmode = localStorage.getItem('darkmode');
+   darkmode !== "active" ? enableDarkmode() : disableDarkmode();
 });
